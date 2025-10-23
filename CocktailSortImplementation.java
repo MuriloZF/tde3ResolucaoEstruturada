1cc2fe0766bf69c8567aa0f8a1a681b62988958e
Murilo Zimerman Fortaleza <mzimermanfortaleza@gmail.com>
2025-10-23 15:35:17 -0300

    Implemented Cocktail Sort

---
+++ CocktailSortImplementation.java
@@ -0,0 +1,62 @@
+class CocktailSort{
+	public static void cocktailsort(int size, int[] list){
+		int swaps = 0;
+		int iteration = 0;
+		int temp;
+		int start = 0;
+		int end = size - 1;
+		long startTime = 0;
+		long elapsedTime = 0;
+		double finalTime = 0;
+		boolean flag = true;
+		startTime = System.nanoTime();
+		while(flag){
+			flag = false;
+			iteration++;
+			for(int i = start; i < end; i++){
+				if(list[i] > list[i + 1]){
+					temp = list[i];
+					list[i] = list[i + 1];
+					list[i + 1] = temp;
+					swaps++;
+					flag = true;		
+				}
+			}
+			if(!flag){
+				break;
+			}
+			flag = false;
+			end--;
+			for(int i = end; i > start; i--){
+				if(list[i] < list[i -1]){
+					temp = list[i];
+					list[i] = list[i - 1];
+					list[i -1] = temp;
+					swaps++;
+					flag = true;
+				}
+			}
+			start++;
+		}
+		elapsedTime = System.nanoTime() - startTime;
+		finalTime = elapsedTime / 1_000_000_000.0;
+		System.out.printf("Ordenado (Cocktail Sort):\n");
+		for(int i = 0; i < size; i++){
+			System.out.printf("%d, ", list[i]);
+		}
+		System.out.printf("\nTrocas: %d,\n", swaps);
+		System.out.printf("Iterações: %d.\n", iteration);
+		System.out.printf("Tempo: %fs.\n", finalTime);
+	} 
+}
+
+public class CocktailSortImplementation{
+	public static void main(String[]args){
+		CreateList createList = new CreateList();
+		CocktailSort cocktailSort = new CocktailSort();
+		int size = 20;
+		cocktailSort.cocktailsort(size, createList.createFirstList());
+		cocktailSort.cocktailsort(size, createList.createSecondList());		
+		cocktailSort.cocktailsort(size, createList.createThirdList());
+	}
+}
